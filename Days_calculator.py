ff2e0320fcffe7b4777cf6de72bbd8cb2ad77738
Chaithanyagowd123 <150154767+Chaithanyagowd123@users.noreply.github.com>
2024-02-18 19:46:47 +0530

    Add files via upload

---
+++ Days_calculator.py
@@ -0,0 +1,8 @@
+num=int(input('enter your age'))
+years_remaining=90-num
+months_remaining=(90-num)*12
+days_remaining=years_remaining*365
+
+print(years_remaining)
+print(months_remaining)
+print(days_remaining)
